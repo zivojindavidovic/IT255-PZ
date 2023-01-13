298a2ed4063be29fdfb9c8d4bf32df03598773a0
zivojindavidovic <zivojin.davidovic2001@gmail.com>
2023-01-13 22:00:38 +0100

    rad-sa-direktivama

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  contactForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){
+    this.validateForm();
+  }
+
+  validateForm(){
+    this.contactForm = this.formBuilder.group({
+      name: ['',[Validators.required, Validators.minLength(5)]],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(20)]]
+    })
+  }
 
 }
