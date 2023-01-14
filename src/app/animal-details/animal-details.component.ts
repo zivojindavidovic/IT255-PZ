413135f1d06c5ae44ca2f18b7be1486baa82b3e6
zivojindavidovic <zivojin.davidovic2001@gmail.com>
2023-01-14 15:06:24 +0100

    edit-itema

--- src/app/animal-details/animal-details.component.ts
+++ src/app/animal-details/animal-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Animal } from '../models/animal';
 import { AnimalService } from '../services/animal.service';
@@ -11,18 +12,40 @@ import { AnimalService } from '../services/animal.service';
 export class AnimalDetailsComponent {
 
   public animal: Animal;
-
+  public editAnimalForm: FormGroup;
   constructor(private _animalService: AnimalService, private _router: ActivatedRoute){}
 
   ngOnInit(): void{
     this._router.params.subscribe(params => {
       let id =+ params['id'];
       this.getAnimal(id);
+    });
+    this.initForm();
+  }
+
+  
+  public initForm(){
+    this.editAnimalForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      price: new FormControl('', Validators.required),
+      image: new FormControl('', Validators.required),
     })
   }
+
+  
   public getAnimal(id: number){
     this._animalService.showAnimal(id).subscribe((data) =>
       this.animal = data  
     );
+  }  
+
+  public editAnimal(id: number){
+    
+    let name = this.editAnimalForm.get('name')?.value;
+    let price = this.editAnimalForm.get('price')?.value;
+    let image = this.editAnimalForm.get('image')?.value;
+    this._animalService.editAnimalById(id, name, price, image).subscribe((data)=>
+      this.animal = data
+    )
   }
 }
--- src/app/services/animal.service.ts
+++ src/app/services/animal.service.ts
@@ -10,7 +10,8 @@ export class AnimalService {
 
   private baseUrl = "http://localhost:3000/animals"
 
-  constructor(private _httpClient: HttpClient) { }
+  constructor(private _httpClient: HttpClient) { 
+  }
 
   public getAnimals(): Observable<Animal[]> {
     return this._httpClient.get<Animal[]>(this.baseUrl).pipe(
@@ -35,8 +36,18 @@ export class AnimalService {
     )
   }
 
+  public editAnimalById(id: number, name: string, price: number, image: string): Observable<Animal>{
+    const body = {name: name, price: price, image: image};
+    return this._httpClient.put<Animal>(this.baseUrl+'/'+id, body).pipe(
+      map((data:any) => this._createAnimalFromObject(data))
+    )
+  }
 
   private _createAnimalFromObject(animal: any): any {
     return new Animal(animal.id, animal.name, animal.price, animal.image)
   }
 }
+
+
+
+ 
